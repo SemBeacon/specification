c8a2a2c91089d196d6721e96fa7edfdb9ab925b3
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2023-01-31 18:56:38 +0100

    feat: generator for geojson to ttl

--- examples/ubicomp2023/transformer/src/generate.ts
+++ examples/ubicomp2023/transformer/src/generate.ts
@@ -1,14 +1,16 @@
 import '@openhps/rdf';
-import { Absolute2DPosition, Absolute3DPosition, DataSerializerUtils } from '@openhps/core';
+import { Absolute3DPosition, MemoryDataService } from '@openhps/core';
 import { CSVDataObjectService } from '@openhps/csv';
-import { Building, SymbolicSpace } from '@openhps/geospatial';
+import { Building, Floor, SymbolicSpace, SymbolicSpaceService } from '@openhps/geospatial';
 import { BLEObject, MACAddress } from '@openhps/rf';
-import { RDFSerializer, Store } from '@openhps/rdf';
+import { IriString, NamedNode, ogc, Quad, RDFSerializer, Store, Term } from '@openhps/rdf';
 
 import * as fs from 'fs';
 import * as path from 'path';
 
 const BASE_URI = "http://sembeacon.org/example/";
+const BEACONS_URI = BASE_URI + "beacons.ttl#" as IriString;
+const BUILDING_URI = BASE_URI + "building.ttl#" as IriString;
 
 async function loadData() {
     const data = {
@@ -23,33 +25,51 @@ async function loadData() {
     );
     const geojson = JSON.parse(geojsonStr);
     const spaces = geojson.features.map(feature => SymbolicSpace.fromGeoJSON(feature));
+    const spaceService = new SymbolicSpaceService(new MemoryDataService(SymbolicSpace));
+    await spaceService.emitAsync('build');
+    await Promise.all(spaces.map(space => spaceService.insertObject(space)));
     data.spaces = await RDFSerializer.stringify(
-        new Store(spaces.map(space => RDFSerializer.serializeToQuads(space, BASE_URI)).reduce((a, b) => [...a, ...b])),
-        { prettyPrint: true, baseUri: BASE_URI });
+        new Store(spaces.map(space => RDFSerializer.serializeToQuads(space, BUILDING_URI)).reduce((a, b) => [...a, ...b])),
+        { prettyPrint: true, baseUri: BUILDING_URI });
 
     console.log("Loading beacon data ...");
-    const service = new CSVDataObjectService(BLEObject, {
+    const building = spaces.filter(space => space instanceof Building)[0];
+    const floor = spaces.filter(space => space instanceof Floor)[0];
+    const beaconService = new CSVDataObjectService(BLEObject, {
         file: path.join(__dirname, "../data/ble_devices.csv"),
         rowCallback: (row: any) => {
             const object = new BLEObject(MACAddress.fromString("00:11:22:33:44"));
             object.uid = row.ID;
             object.displayName = row.ID;
-            const building = spaces.filter(space => space instanceof Building)[0];
-            object.setPosition(building.transform(new Absolute3DPosition(
-                parseFloat(row.X),
-                parseFloat(row.Y),
-                1.6
-            )));
-            console.log(object.getPosition());
+            object.setPosition(
+                building.transform(
+                    floor.transform(new Absolute3DPosition(
+                        parseFloat(row.X),
+                        parseFloat(row.Y),
+                        1.6
+                    ))
+                ));
             return object;
         }
     });
-    await service.emitAsync('build');
-    const beacons: BLEObject[] = await service.findAll();
+    await beaconService.emitAsync('build');
+    const beacons: BLEObject[] = await beaconService.findAll();
     data.beacons = await RDFSerializer.stringify(
-        new Store(beacons.map(beacon => RDFSerializer.serializeToQuads(beacon, BASE_URI)).reduce((a, b) => [...a, ...b])),
-        { prettyPrint: true, baseUri: BASE_URI });
+        new Store(await beacons.map(async beacon => {
+            const quads = RDFSerializer.serializeToQuads(beacon, BEACONS_URI);
+
+            // Add information on where a beacon is placed
+            const candidates = await spaceService.findSymbolicSpaces(beacon.getPosition());
+            if (candidates.length > 0) {
+                const deployment = RDFSerializer.serializeToQuads(candidates[0][0], BUILDING_URI)[0];
+                quads.push(new Quad(quads[0].subject, new NamedNode(ogc.sfWithin), deployment.subject))
+            }
+
+            return quads;
+        }).reduce(async (a, b) => [...await a, ...await b])),
+        { prettyPrint: true, baseUri: BEACONS_URI });
     
+    console.log("Saving RDF data ...");
     const dir = path.join(__dirname, "../dist/");
     if (!fs.existsSync(dir))
         fs.mkdirSync(dir);
